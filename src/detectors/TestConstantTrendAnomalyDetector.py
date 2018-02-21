4bc5746dd1b224b29eb0d20731fc9c08c74d122c
Alpha <alphagma@gmail.com>
2018-02-21 12:02:26 -0700

    Improved tests based on PR feedback (#3)

--- src/detectors/TestConstantTrendAnomalyDetector.py
+++ src/detectors/TestConstantTrendAnomalyDetector.py
@@ -14,31 +14,30 @@ class TestConstantTrendAnomalyDetector(unittest.TestCase):
         self.assertTrue(sut.is_model_suitable(np.repeat(5, 100)))
         self.assertTrue(sut.is_model_suitable(np.repeat(5, 1)))
         self.assertTrue(sut.is_model_suitable(np.repeat(1, 1)))
+        self.assertTrue(sut.is_model_suitable([1.1, 1.1]))
+        self.assertTrue(sut.is_model_suitable(['test string', 'test string']))
 
-    def test_is_model_suitable_(self):
+    def test_is_model_suitable_invalid_cases(self):
         sut = self.__get_detector()
 
-        self.assertFalse(sut.is_model_suitable(np.array([1, 2])))
-        self.assertFalse(sut.is_model_suitable(np.array([0, 2])))
+        self.assertFalse(sut.is_model_suitable(['a', 'b']))
+        self.assertFalse(sut.is_model_suitable([1.1, 1.2]))
+        self.assertFalse(sut.is_model_suitable([1, 1, 2]))
+        self.assertFalse(sut.is_model_suitable([1, 2]))
+        self.assertFalse(sut.is_model_suitable([0, 2]))
 
-    def test_train_handles_arrays(self):
+    def __is_anomalous_from_dataset(self, dataset, test_value):
         sut = self.__get_detector()
 
-        sut.train([1, 1])
-        sut.train([1])
-        sut.train([0])
-        sut.train(np.ones(100))
-
-    def __train_and_assert_is_anomalous_false(self, values, test_value):
-        sut = self.__get_detector()
-
-        sut.train(values)
-        self.assertFalse(sut.is_anomalous(test_value))
+        sut.train(dataset)
+        return sut.is_anomalous(test_value)
 
     def test_is_anomalous_returns_false_on_same_value(self):
-        self.__train_and_assert_is_anomalous_false(np.repeat(1, 1), 1)
-        self.__train_and_assert_is_anomalous_false(np.repeat(1, 2), 1)
-        self.__train_and_assert_is_anomalous_false(np.repeat(5, 100), 5)
+        self.assertFalse(self.__is_anomalous_from_dataset([1], 1))
+        self.assertFalse(self.__is_anomalous_from_dataset([1, 1], 1))
+        self.assertFalse(self.__is_anomalous_from_dataset(np.repeat(5, 100), 5))
+        self.assertFalse(self.__is_anomalous_from_dataset(np.repeat('test string', 5), 'test string'))
+        self.assertFalse(self.__is_anomalous_from_dataset([1.1, 1.1], 1.1))
 
     def __train_and_assert_is_anomalous_true(self, values, test_value):
         sut = self.__get_detector()
@@ -47,9 +46,11 @@ class TestConstantTrendAnomalyDetector(unittest.TestCase):
         self.assertTrue(sut.is_anomalous(test_value))
 
     def test_is_anomalous_returns_true_on_different_value(self):
-        self.__train_and_assert_is_anomalous_true(np.repeat(1, 1), 2)
-        self.__train_and_assert_is_anomalous_true(np.repeat(1, 2), 2)
-        self.__train_and_assert_is_anomalous_true(np.repeat(5, 100), 6)
+        self.assertTrue(self.__is_anomalous_from_dataset([1], 2))
+        self.assertTrue(self.__is_anomalous_from_dataset([1, 1], 2))
+        self.assertTrue(self.__is_anomalous_from_dataset(np.repeat(5, 100), 6))
+        self.assertTrue(self.__is_anomalous_from_dataset(np.repeat('test string', 5), 'second test string'))
+        self.assertTrue(self.__is_anomalous_from_dataset([1.1, 1.1], 1.2))
 
 if __name__ == '__main__':
     unittest.main()
